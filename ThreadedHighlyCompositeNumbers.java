3c15c32df65c98c9782eb7d86bdb1aa8b1e05a84
Connor Baker <ConnorBaker01@Gmail.com>
2016-07-16 20:19:34 -0400

    ThreadedHighlyCompositeNumbers: Update to 0.5a
    Get rid of a variable that I didn't really need.

--- ThreadedHighlyCompositeNumbers.java
+++ ThreadedHighlyCompositeNumbers.java
@@ -1,21 +1,17 @@
 /*
 *  Author: Connor Baker
-*  Version: 0.4a
+*  Version: 0.5a
 *  Created: July 16, 2016
 *  Last Updated: July 16, 2016
 *
 *  Description: Compute highly composite numbers.
 *
 *  Note: I suspect that all arbitrarily large n that we are searching for end
-*  in a zero. As such, we could iterated by 10 each time.
-*
-*  TODO: Find a way to reuse code. I hate copy-pasting code.
-*
-*  TODO: Find out if I can reuse variable names between classes.
+*  in a zero. As such, I've decided to add 10 each iteration and skip testing
+*  inbetween values.
 */
 
-// Import necessary packages
-import java.lang.Thread;
+/* Import necessary packages */
 import java.math.BigInteger;
 
 public class ThreadedHighlyCompositeNumbers {
@@ -37,14 +33,12 @@ class Compute0 extends Thread {
     BigInteger divisor0 = BigInteger.valueOf(2);
     BigInteger number_of_divisors0 = BigInteger.valueOf(0);
     BigInteger temp_number_of_divisors0 = BigInteger.valueOf(0);
-    BigInteger smallest_n_for_largest_number_of_divisors0 = BigInteger.valueOf(1);
 
-    while (true) {
+    while (n0.intValue()<5038) {
       if (divisor0.compareTo(n0.divide(BigInteger.valueOf(2))) == 1) {
         if ((temp_number_of_divisors0.compareTo(number_of_divisors0)) == 1) {
           number_of_divisors0 = temp_number_of_divisors0;
-          smallest_n_for_largest_number_of_divisors0 = n0;
-          System.out.println(smallest_n_for_largest_number_of_divisors0+ " has " +number_of_divisors0+ " divisors.");
+          System.out.println(n0+ " has " +number_of_divisors0+ " divisors.");
         }
         temp_number_of_divisors0 = BigInteger.valueOf(0);
         divisor0 = BigInteger.valueOf(2);
@@ -67,14 +61,12 @@ class Compute1 extends Thread {
     BigInteger divisor1 = BigInteger.valueOf(2);
     BigInteger number_of_divisors1 = BigInteger.valueOf(0);
     BigInteger temp_number_of_divisors1 = BigInteger.valueOf(0);
-    BigInteger smallest_n_for_largest_number_of_divisors1 = BigInteger.valueOf(1);
 
-    while (true) {
+    while (n1.intValue()<5039) {
       if (divisor1.compareTo(n1.divide(BigInteger.valueOf(2))) == 1) {
         if ((temp_number_of_divisors1.compareTo(number_of_divisors1)) == 1) {
           number_of_divisors1 = temp_number_of_divisors1;
-          smallest_n_for_largest_number_of_divisors1 = n1;
-          System.out.println(smallest_n_for_largest_number_of_divisors1+ " has " +number_of_divisors1+ " divisors.");
+          System.out.println(n1+ " has " +number_of_divisors1+ " divisors.");
         }
         temp_number_of_divisors1 = BigInteger.valueOf(0);
         divisor1 = BigInteger.valueOf(2);
@@ -97,14 +89,12 @@ class Compute2 extends Thread {
     BigInteger divisor2 = BigInteger.valueOf(2);
     BigInteger number_of_divisors2 = BigInteger.valueOf(0);
     BigInteger temp_number_of_divisors2 = BigInteger.valueOf(0);
-    BigInteger smallest_n_for_largest_number_of_divisors2 = BigInteger.valueOf(1);
 
-    while (true) {
+    while (n2.intValue()<5040) {
       if (divisor2.compareTo(n2.divide(BigInteger.valueOf(2))) == 1) {
         if ((temp_number_of_divisors2.compareTo(number_of_divisors2)) == 1) {
           number_of_divisors2 = temp_number_of_divisors2;
-          smallest_n_for_largest_number_of_divisors2 = n2;
-          System.out.println(smallest_n_for_largest_number_of_divisors2+ " has " +number_of_divisors2+ " divisors.");
+          System.out.println(n2+ " has " +number_of_divisors2+ " divisors.");
         }
         temp_number_of_divisors2 = BigInteger.valueOf(0);
         divisor2 = BigInteger.valueOf(2);
@@ -127,14 +117,12 @@ class Compute3 extends Thread {
     BigInteger divisor3 = BigInteger.valueOf(2);
     BigInteger number_of_divisors3 = BigInteger.valueOf(0);
     BigInteger temp_number_of_divisors3 = BigInteger.valueOf(0);
-    BigInteger smallest_n_for_largest_number_of_divisors3 = BigInteger.valueOf(1);
 
-    while (true) {
+    while (n3.intValue()<5041) {
       if (divisor3.compareTo(n3.divide(BigInteger.valueOf(2))) == 1) {
         if ((temp_number_of_divisors3.compareTo(number_of_divisors3)) == 1) {
           number_of_divisors3 = temp_number_of_divisors3;
-          smallest_n_for_largest_number_of_divisors3 = n3;
-          System.out.println(smallest_n_for_largest_number_of_divisors3+ " has " +number_of_divisors3+ " divisors.");
+          System.out.println(n3+ " has " +number_of_divisors3+ " divisors.");
         }
         temp_number_of_divisors3 = BigInteger.valueOf(0);
         divisor3 = BigInteger.valueOf(2);

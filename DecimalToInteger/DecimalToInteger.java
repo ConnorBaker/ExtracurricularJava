49cef8b5abde0e2adc66bb00d348f9f50d6658b2
Connor Baker <ConnorBaker01@gmail.com>
2016-11-30 18:03:12 -0500

    DecimalToInteger: v0.2a

--- DecimalToInteger/DecimalToInteger.java
+++ DecimalToInteger/DecimalToInteger.java
@@ -1,48 +1,39 @@
 /*
 *  filename: DecimalToInteger.java
-*  author: Connor Baker
-*  version: 0.1f
+*  author: Connor Baker, IllegalArgument
+*  version: 0.2a
 *  description: Convert a decimal entered by user (between 0 and 1) into its string
 *  representation (e.g. 0.123 becomes "0.123").
+*  references: The ever knowledgeable IllegalArgument of chatroom legends (plural)
 */
 
 import java.util.Scanner;
-import java.lang.Math;
-import java.lang.Character;
 
 public class DecimalToInteger {
-  // double decimal;
-  // int length;
-  // long longRepresenation;
-  // String stringRepresentation;
+// Initialize all of our variables
+int length; // the length of the decimal (excluding the leading zero)
+int i; // an iteration counter
+long l; // the value of
+long[] a; // holds the values of the different places of the decimal
+double decimal; // the value of the user-inputted decimal
+char[] c; // holds the character representation of the decimal
+String stringRepresentation; // the string representation of the decimal
 
-  // long[] arrayOfLongs;
-  // char arrayOfChars[];
-
+  // Decimal constructor
   DecimalToInteger() {
-    long l = 54325;
-    long[] a = new long[5];
-    int i = 0;
+    getDecimal();
+    getLength();
+    getLong();
+    a = new long[length];
+    c = new char[length+2]; // Contains "0." in addition to all of a[]
+    i = 0;
     computeDigits(l, a, i);
-  //   getDecimal();
-  //   getLength();
-  //   getLong();
-  //   System.out.println(decimal);
-  //   System.out.println(length);
-  //   System.out.println(longRepresenation);
-    // arrayOfLongs = new long[length];
-    // getArrayOfLongs();
-    // char arrayOfChars[] = new char[length+2]; // +2 because it holds the '0.' part
-    // getArrayOfChars();
-    // getStringRepresentation();
-    // printStringRepresentation();
+    getArrayOfChars();
+    getStringRepresentation();
+    printStringRepresentation();
   }
 
-
-  public static void main(String args[]) {
-    DecimalToInteger newDecimal = new DecimalToInteger();
-  }
-
+  // Method to compute the long representation of the user-inputted decimal
   public void computeDigits(long l, long[] a, int i) {
     a[i] = l % 10L;
     System.out.println("a["+i+"] = "+l+" % "+10L+" = "+a[i]);
@@ -51,89 +42,123 @@ public class DecimalToInteger {
     }
   }
 
-  // public void getDecimal() {
-  //   // Create a scanner to grab user-inputted decimal
-  //   Scanner grab = new Scanner(System.in);
-  //
-  //   // Prompt the user for the decimal and grab it
-  //   System.out.println("Input decimal");
-  //   decimal = grab.nextDouble();
-  //
-  //   // Close the Scanner
-  //   grab.close();
-  // }
-  //
-  // public void getLength() {
-  //   // Initialize our counter
-  //   length = 0;
-  //   double tempDecimal = decimal;
-  //
-  //   // Get the length of the decimal
-  //   while ((tempDecimal%1) != 0) {
-  //     tempDecimal *= 10;
-  //     System.out.println("tempDecimal is "+tempDecimal);
-  //     length++;
-  //   }
-  // }
-  //
-  // public void getLong() {
-  //   // Initialize the variable that will hold the content of the decimal
-  //   longRepresenation = 0;
-  //   double tempDecimal = decimal;
-  //
-  //   // Get the representation of the decimal
-  //   while ((tempDecimal%1) != 0) {
-  //     tempDecimal *= 10;
-  //   }
-  //   longRepresenation = (long)tempDecimal;
-  // }
+  // Method to prompt user for decimal and grab the input
+  public void getDecimal() {
+    // Create a scanner to grab user-inputted decimal
+    Scanner grab = new Scanner(System.in);
 
-  // public void getStringRepresentation() {
-  //   stringRepresentation = new String(arrayOfChars);
-  // }
-  //
-  // public void printStringRepresentation() {
-  //   System.out.println(stringRepresentation);
-  // }
+    // Prompt the user for the decimal and grab it
+    System.out.println("Input decimal");
+    decimal = grab.nextDouble();
 
-  public void getArrayOfChars() {
-    // Fill the first two entires of the array with the "0." part of the decimal
-    arrayOfChars[0] = '0';
-    arrayOfChars[1] = '.';
+    // Close the Scanner
+    grab.close();
+  }
 
-    // Fill the array of characters using the array of integers
-    for (int i = 2; i < length+2; i++) {
-      arrayOfChars[i] = getCharacterAtIndex(i-2);
+  // Method to get the length of the user-inputted decmal (excluding the leading zero)
+  public void getLength() {
+    // Initialize the value of the length to zero
+    length = 0;
+
+    // Create a copy of decimal so we don't change the actual value
+    double tempDecimal = decimal;
+
+    // Calculate the length of the decimal
+    /*
+    *  Java allows us to do modular arithmatic on floating point values. We encounter a
+    *  problem with certain decimal inputs because they have no finite represenation as a
+    *  "decimal" in base 2. When this occurs, we get a length of 17.
+    */
+    while ((tempDecimal%1) != 0) {
+      tempDecimal *= 10;
+      length++;
     }
   }
 
+  // Method to get the long representation of the user-inputted decimal
+  public void getLong() {
+    // Initialize the variable that will hold the content of the decimal
+    l = 0;
+    double tempDecimal = decimal;
+
+    // Get the representation of the decimal
+    while ((tempDecimal%1) != 0) {
+      tempDecimal *= 10;
+    }
+    l = (long)tempDecimal;
+  }
+
+  // Method to get the string representation of the user-inputted decimal
+  // We create a string using our character array. Strings in Java are just immutable
+  // arrays of characters.
+  public void getStringRepresentation() {
+    stringRepresentation = new String(c);
+  }
+
+  // Method to print the string representation of the user-inputted decimal
+  public void printStringRepresentation() {
+    System.out.println("The string representation of the entered decimal is "
+        +stringRepresentation+".");
+    System.out.println("Due to java storing all doubles in base 2, the accuracy of what"
+        +" you've inputted is lost for some numbers.");
+    System.out.println("For example, 0.1 has no finite representation (as base 2's"
+        +" equivalent of a decimal) in base 2.");
+  }
+
+  // Method to create an array of characters representing the user-inputted decimal
+  public void getArrayOfChars() {
+    // Fill the first two entires of the array with the "0." part of the decimal
+    c[0] = '0';
+    c[1] = '.';
+    // System.out.println("c[0] = "+c[0])+"\nc[1] = "+c[1]);
+
+    // Fill the array of characters using the array of integers
+    for (int i = 2; i < (length+2); i++) {
+      /*
+      *  By using (2-i)+(length-1) we create a mapping from the first value of a[] to the
+      *  last value of c[]. We must do this because we read in the values of
+      *  the decimal effectively in reverse (starting at the least significant bit).
+      */
+      c[i] = getCharacterAtIndex((2-i)+(length-1));
+      // System.out.println("c["+i+"] = "+c[i]);
+    }
+  }
+
+  // Method to find the character representation of the integer at a given index of a[]
   public char getCharacterAtIndex(int i) {
     // Initialize our character
     char character = '0';
 
     // Find the character representation of the integer
-    if (arrayOfLongs[i] == 0) {
+    if (a[i] == 0) {
       character = '0';
-    } else if (arrayOfLongs[i] == 1) {
+    } else if (a[i] == 1) {
       character = '1';
-    } else if (arrayOfLongs[i] == 2) {
+    } else if (a[i] == 2) {
       character = '2';
-    } else if (arrayOfLongs[i] == 3) {
+    } else if (a[i] == 3) {
       character = '3';
-    } else if (arrayOfLongs[i] == 4) {
+    } else if (a[i] == 4) {
       character = '4';
-    } else if (arrayOfLongs[i] == 5) {
+    } else if (a[i] == 5) {
       character = '5';
-    } else if (arrayOfLongs[i] == 6) {
+    } else if (a[i] == 6) {
       character = '6';
-    } else if (arrayOfLongs[i] == 7) {
+    } else if (a[i] == 7) {
       character = '7';
-    } else if (arrayOfLongs[i] == 8) {
+    } else if (a[i] == 8) {
       character = '8';
-    } else if (arrayOfLongs[i] == 9) {
+    } else if (a[i] == 9) {
       character = '9';
     }
 
+    // Return the character to the method caller
     return character;
+  }
+
+  // main method
+  public static void main(String args[]) {
+    // Create our new object
+    DecimalToInteger newDecimal = new DecimalToInteger();
   }
 }

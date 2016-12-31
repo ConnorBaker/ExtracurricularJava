ffb5f707b7b3d0d166c695dcc846f107e1fb0f4a
Connor Baker <connorbaker01@gmail.com>
2016-12-31 15:27:55 -0500

    Base10DecimalToBinary4: v0.1a

---
+++ Base10DecimalToBinary/Base10DecimalToBinary4.java
@@ -0,0 +1,140 @@
+/*
+*  filename: Base10DecimalToBinary4.java
+*  author: Connor Baker
+*  version: 0.1a
+*  description: Convert a decimal entered by user (between 0 and 1) into its
+*  binary representation (e.g. 0.625 becomes "0.101"). Allows for arbitrary
+*  precision given that there is enough memory allowed in the stack. The stack
+*  size can be changed by executing the program using "java -Xss8g
+*  Base10DecimalToBinary" where 8g allocated eight gigabytes of memory to the
+*  stack.
+*  Uses some neat math tricks.
+*  references: http://cs.furman.edu/digitaldomain/more/ch6/dec_frac_to_bin.htm
+*/
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Base10DecimalToBinary4 {
+  // Declare the objects used throughout the program
+  final static boolean debugging = false;
+  long count = 0;
+  long numberOfDecimalPlaces=0;
+  long numberOfFiguresToTrack;
+  BigDecimal decimal;
+  final static BigDecimal VALUEOFZERO = new BigDecimal("0.0");
+  final static BigDecimal VALUEOFONE = new BigDecimal("1.0");
+  final static BigDecimal VALUEOFTWO = new BigDecimal("2.0");
+  final static BigDecimal VALUEOFTHREE = new BigDecimal("3.0");
+  final static BigDecimal VALUEOFFOUR = new BigDecimal("4.0");
+  ArrayList<Character> binaryRepresentation = new ArrayList<>();
+  Scanner grabber = new Scanner(System.in);
+
+  // Default, no-arg constructor
+  Base10DecimalToBinary4() {
+    initialization();
+    queryForAccuracy();
+    binaryRepresentation.add('0');
+    binaryRepresentation.add('.');
+    decimalFractionToBinary(decimal);
+  }
+
+  // Method to initialize the object
+  public void initialization() {
+    System.out.println("Input the decimal fraction of your choice between 0"
+      + " and 1 (exclusive)");
+    decimal = new BigDecimal(grabber.nextLine());
+  }
+
+  public void queryForAccuracy() {
+    System.out.println("Input the number of figures you want to track");
+    numberOfFiguresToTrack = grabber.nextLong();
+  }
+
+  public void decimalFractionToBinary(BigDecimal decimal) {
+    // Check to see if the program needs to stop
+    if (numberOfDecimalPlaces >= numberOfFiguresToTrack) {
+      System.out.println("There were "+count+" iterations");
+      System.out.println(binaryRepresentation);
+      return;
+    }
+
+    // Multiply the decimal by 4 to test for new additions to the binary
+    // representation
+    decimal = decimal.multiply(VALUEOFFOUR);
+
+    // First case where the decimal is less than one
+    // I use intValue here because I beleive it's faster than compareTo (it
+    // truncates the decimal portion for us)
+    if (decimal.intValue() == 0) {
+      if (debugging == true) {
+         System.out.println("decimal.intValue() == 0");
+       }
+      binaryRepresentation.add('0');
+      binaryRepresentation.add('0');
+      numberOfDecimalPlaces += 2;
+    }
+
+    // Second case where the decimal is greater than one, but less than two
+    else if (decimal.intValue() == 1) {
+      if (debugging == true) {
+         System.out.println("decimal.intValue() == 1");
+       }
+      decimal = decimal.subtract(VALUEOFONE);
+      binaryRepresentation.add('1');
+      numberOfDecimalPlaces++;
+    }
+
+    // Third case where the decimal is greater than two, but less than three
+    else if (decimal.intValue() == 2) {
+      if (debugging == true) {
+         System.out.println("decimal.intValue() == 2");
+       }
+      decimal = decimal.subtract(VALUEOFTWO);
+      binaryRepresentation.add('1');
+      binaryRepresentation.add('0');
+      numberOfDecimalPlaces += 2;
+    }
+
+    // Fourth case where the decimal is greater than three, but less than four
+    else if (decimal.intValue() == 3) {
+      if (debugging == true) {
+         System.out.println("decimal.intValue() == 3");
+       }
+      decimal = decimal.subtract(VALUEOFTHREE);
+      binaryRepresentation.add('1');
+      binaryRepresentation.add('1');
+      numberOfDecimalPlaces += 2;
+    }
+
+    // Fifth case where there is no remainder for the decimal and it has a
+    // finite representation as a 'decimal' in base two
+    else if (decimal.compareTo(VALUEOFZERO) == 0) {
+      if (debugging == true) {
+        System.out.println("decimal.compareTo(VALUEOFZERO) == 0");
+      }
+      System.out.println(binaryRepresentation);
+      System.out.println("Conversion has finite result above");
+      return;
+    }
+
+    // Sixth case where something stupid and werid happens
+    else {
+      if (debugging == true) {
+        System.out.println("Something weird is happening!");
+      }
+    }
+
+    // Increment the execution counter by four since we mutiplied by four
+    count++;
+
+    // Pass the decimal back into the method to run again
+    decimalFractionToBinary(decimal);
+  }
+
+  // Main method
+  public static void main(String[] args) {
+    Base10DecimalToBinary4 newConversion = new Base10DecimalToBinary4();
+  }
+}
